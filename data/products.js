88fea70c15d9e8c204e63752cd8f68024864aaff
abhishek264 <prasadabhishek264@gmail.com>
2022-07-15 11:29:25 +0530

    first commit

---
+++ data/products.js
@@ -0,0 +1,77 @@
+const products = [
+  {
+    name: "boAt Airdopes 121v2 TWS Earbuds",
+    image: "/images/boatHeadfone.jpg",
+    description:
+      "boAt Airdopes 121v2 TWS Earbuds with Bluetooth V5.0, Immersive Audio, Up to 14H Total Playback, Instant Voice Assistant, Easy Access Controls with Mic and Dual Tone Ergonomic Design(Active Black) ",
+    brand: "Boat",
+    category: "Electronics",
+    price: 20.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: "Micromax IN 1b (Purple, 32 GB)",
+    image: "/images/micromaxInB.jpg",
+    description:
+      "Say hello to the Micromax IN 1b smartphone whose powerful MediaTek Helio G35 gaming processor and a 5000 mAh battery will pave the way for effortless multitasking and usage.",
+    brand: "Micromax",
+    category: "Electronics",
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: "Cannon EOS 80D DSLR Camera",
+    image: "/images/camera.jpg",
+    description:
+      "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
+    brand: "Cannon",
+    category: "Electronics",
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    name: "Simpson Gravity fall t-shirt",
+    image: "/images/Gravity_Simpson.jpg",
+    description:
+      "Regular fit t-shirt",
+    brand: "Sony",
+    category: "cloth",
+    price: 399.99,
+    countInStock: 11,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    name: "Logitech G-Series Gaming Mouse",
+    image: "/images/mouse.jpg",
+    description:
+      "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: "Spiderman Shirt",
+    image: "/images/Spiderman.jpg",
+    description:
+      "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
+    brand: "Spiderman",
+    category: "Clothes",
+    price: 29.99,
+    countInStock: 2,
+    rating: 4.5,
+    numReviews: 12,
+  },
+];
+
+module.exports = products;
+
